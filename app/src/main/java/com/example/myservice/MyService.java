cdb727979c6ee85347cb327c325e753abb97eded
plabandas <turjoyplaban09@gmail.com>
2021-12-19 22:41:24 +0600

    Delay Sometimes by using Background Thread inside Main Thread

--- app/src/main/java/com/example/myservice/MyService.java
+++ app/src/main/java/com/example/myservice/MyService.java
@@ -2,10 +2,13 @@ package com.example.myservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class MyService extends Service {
+
+    private Handler handler;
     public MyService() {
     }
 
@@ -18,19 +21,30 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
 
+        super.onCreate();
         Toast.makeText(this, "On Creat Called",Toast.LENGTH_SHORT).show();
 
-        super.onCreate();
+        handler= new Handler() ;
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyService.this, "Service WokeUp", Toast.LENGTH_SHORT).show();
+            }
+        },7000);
+        
 
         Toast.makeText(this, "Servicing is Running", Toast.LENGTH_SHORT).show();
 
         stopSelf();   //Stop service automatic after working
         return super.onStartCommand(intent, flags, startId);
     }
+
 
     @Override
     public void onDestroy() {
